46ca90619cd60e7747726f4829a792712e8f58fa
Bennok <bennnok@gmail.com>
2024-06-03 13:25:02 +0200

    feature: update code comments

--- 03_DBinCSV.py
+++ 03_DBinCSV.py
@@ -14,22 +14,22 @@ csv_file_path = "./csv/artikel.csv"
 # gibt nach erfolgreicher Verbindung mit der Datenbank, die Sql-Abfrage zurück
 def testprint(sql):
     try:
-        cursor.execute(sql)
-        rows = cursor.fetchall()
-
         result = list()
         column_names = list()
 
-        for columns in cursor.description:
+        cursor.execute(sql) #execute sql-query and get result
+        rows = cursor.fetchall()
+
+        for columns in cursor.description: #get column names
             column_names.append(columns[0])
 
-        result.append(column_names)
+        result.append(column_names) #get row contents
         for row in rows:
             result.append(row)
 
-        os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
+        os.makedirs(os.path.dirname(csv_file_path), exist_ok=True) #create directory for csv files
         
-        with open(csv_file_path, 'w', newline='') as csvfile:
+        with open(csv_file_path, 'w', newline='') as csvfile: #write the csv file
             csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for row in result:
                 csvwriter.writerow(row)
