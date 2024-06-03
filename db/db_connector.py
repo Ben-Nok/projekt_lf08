3fcd633cc01e9e1017dc0d6f3acce7f08387a3a5
Bennok <bennnok@gmail.com>
2024-06-03 11:58:47 +0200

    feature: add task 04, update dbconnector

--- db/db_connector.py
+++ db/db_connector.py
@@ -4,11 +4,15 @@ from db import db_config
 class DbConnector():
 
     def db_connect(self):
-        return mariadb.connect(
-            user = db_config.user,
-            password = db_config.password,
-            host = db_config.host,
-            port = db_config.port,
-            database = db_config.database
-        )
-
+        try:
+            conn = mariadb.connect(
+                user = db_config.user,
+                password = db_config.password,
+                host = db_config.host,
+                port = db_config.port,
+                database = db_config.database
+            )
+            return conn
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB: {e}")
+            return None
