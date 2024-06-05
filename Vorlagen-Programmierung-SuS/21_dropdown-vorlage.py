10eca9789796e4e37205229d56d717e63e21ea96
Bennok <bennnok@gmail.com>
2024-06-05 08:30:23 +0200

    feature: restructure project files

--- Vorlagen-Programmierung-SuS/21_dropdown-vorlage.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-from tkinter import ttk #Grafik-Bib
-import subprocess # Andere Pythonprgramme ausführen
-
-mitarbeiter_options=["Lager", "Verwaltung", "Marketing", "Geschäftsführung"] # Nutzer Gruppenspezifisch für Login
